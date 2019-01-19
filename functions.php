1eaa55add93a518ea3e01291613474360b38c324
nawras92 <45876701+nawras92@users.noreply.github.com>
2019-01-19 17:43:20 +0200

    Edit 1 item in the cart filter

--- functions.php
+++ functions.php
@@ -2,13 +2,11 @@
  Answer Source:
 https://stackoverflow.com/questions/21363268/need-woocommerce-to-only-allow-1-product-in-the-cart-if-a-product-is-already-in
 */
-add_filter( 'woocommerce_add_cart_item_data', 'woo_custom_add_to_cart' );
+add_filter( 'woocommerce_add_cart_item_data', '_empty_cart' );
 
-function woo_custom_add_to_cart( $cart_item_data ) {
+    function _empty_cart( $cart_item_data ) {
 
-    global $woocommerce;
-    $woocommerce->cart->empty_cart();
+        WC()->cart->empty_cart();
 
-    // Do nothing with the data and return
-    return $cart_item_data;
-}
+        return $cart_item_data;
+    }
