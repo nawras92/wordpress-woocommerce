c53ee6535b7c9fc2369cd5b8fe8e3fc1d6e7a364
nawras92 <45876701+nawras92@users.noreply.github.com>
2019-01-19 17:39:03 +0200

    Create functions.php
    add filter that allows adding one item in the cart.

---
+++ functions.php
@@ -0,0 +1,11 @@
+// Allow one item in the cart
+add_filter( 'woocommerce_add_cart_item_data', 'woo_custom_add_to_cart' );
+
+function woo_custom_add_to_cart( $cart_item_data ) {
+
+    global $woocommerce;
+    $woocommerce->cart->empty_cart();
+
+    // Do nothing with the data and return
+    return $cart_item_data;
+}
