02c2045eaf8d60978f20ae7133a0509549ab9bec
nawras92 <45876701+nawras92@users.noreply.github.com>
2019-01-19 17:40:08 +0200

    Add source to 1 item per cart filter

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
-// Allow one item in the cart
+/* Allow one item in the cart
+ Answer Source:
+https://stackoverflow.com/questions/21363268/need-woocommerce-to-only-allow-1-product-in-the-cart-if-a-product-is-already-in
+*/
 add_filter( 'woocommerce_add_cart_item_data', 'woo_custom_add_to_cart' );
 
 function woo_custom_add_to_cart( $cart_item_data ) {
